ff090491688d2ac34b2264c3f3dfd6a862686db9
piyoppi <garakuta.toolbox@gmail.com>
2022-04-22 11:57:13 +0900

    fix upload-key script

--- bin/upload-key.sh
+++ bin/upload-key.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-eval `cat ./.define.env`
-
 BASEPATH=`dirname $0`
-if [ ! -e $BASEPATH/$ENVFILE ]; then
+
+eval `cat $BASEPATH/.define.env`
+
+if [ ! -e $BASEPATH/../$ENVFILE ]; then
     echo '[ERROR] config file (bin/.env) is not found.'
     exit 255
 fi
@@ -12,5 +13,7 @@ if [ ! -e $1 ]; then
     echo '[ERROR] key file is not found.'
     exit 255
 fi
+
+eval `cat $BASEPATH/../$ENVFILE`
 
 aws s3 cp $1 s3://$CONFIG_BUCKET_NAME
